9baf51c65d99f6b567aad914f77a8dc8090528d2
GigaOwO <takumagiga1118@gmail.com>
2025-11-14 22:06:27 +0900

    ABC_044

---
+++ ABC/ABC_044/A/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var N, K, X, Y int
+	fmt.Scan(&N, &K, &X, &Y)
+	if N <= K {
+		fmt.Println(N * X)
+	} else {
+		fmt.Println(K * X + (N - K) * Y)
+	}
+}
